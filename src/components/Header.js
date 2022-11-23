8f9d3c519e2766b3fe1ab2dac941a8fe5c4690a0
jeffmlb91 <jeffmlb91@gmail.com>
2022-11-23 01:31:32 -0500

    create header links and shopping cart basket

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
+import { 
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
@@ -16,17 +21,32 @@ function Header() {
             />
           </div>
 
-           {/** Search bar */}
-          <div>
-            <input type="text" />
-            
+          {/** Search bar */}
+          <div className=" mx-6 hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500"> 
+            <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
+            <SearchIcon className="h-12 p-4"/>
+          </div>
+          {/**Right side of the header */}
+          <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+            <div className=" link">
+              <p>Hello Jeff MLB</p>
+              <p className="font-extrabold md:text-sm">Account & List</p>
+            </div>
+            <div className=" link">
+              <p>Returns</p>
+              <p className="font-extrabold md:text-sm">& Orders</p>
+            </div>
+            <div className="relative link flex items-center">
+
+              <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+              <ShoppingCartIcon className="h-10"/>
+              <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+            </div>
           </div>
         </div>
 
-         {/*Bottom Nav */}
-        <div>
-          
-        </div>
+        {/*Bottom Nav */}
+        <div></div>
     </header>
   )
 }
