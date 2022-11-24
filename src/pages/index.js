40c14bf2700b97f138103db368b200ccc7920a93
jeffmlb91 <jeffmlb91@gmail.com>
2022-11-24 00:01:03 -0500

    create banner and carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header"
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
       {/* HEADER  */}
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/** Barner */}
+        <Banner />
+        {/** ProductFeed */}
+      </main>
       
     </div>
   );
